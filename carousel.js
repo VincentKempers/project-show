700461eeaa521a9bd3419c8dcc0cd17ec90a043a
VincentKempers <me@vncn.tk>
2020-06-21 12:46:00 +0200

    add element to show carousel-black

--- carousel.js
+++ carousel.js
@@ -1,3 +1,27 @@
 var section = document.querySelector('#section-carousel').children;
+var carousel = document.querySelector('.actual-carousel');
+var count = 0;
 
-console.log(section);
+console.log(carousel);
+console.log(section.length);
+
+function showAllImages() {
+  console.log('imma do it like Eli');
+  carousel.style.display = 'block';
+  carousel.style.opacity = '1';
+
+}
+
+
+function loop() {
+  for (let i=0; i < section.length; i++) {
+    section[i].addEventListener('click', (event) => {
+      console.log(section[i]);
+      showAllImages();
+    },true)
+  };
+}
+
+
+
+loop();
